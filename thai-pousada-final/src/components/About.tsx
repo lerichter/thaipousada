dfd251c88dc97dc57200bb852d402887707baaa4
lerichter <lerichter.c@gmail.com>
2024-01-23 15:03:57 -0300

    fix about

--- thai-pousada-final/src/components/About.tsx
+++ thai-pousada-final/src/components/About.tsx
@@ -50,35 +50,37 @@ export const metadata: Metadata = {
 
 export function About() {
   return (
-    <Container className="mt-16">
-      <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12 bg-custom-white">
-        <div className="lg:pl-20">
-          <div className="max-w-xs px-2.5 lg:max-w-none">
-            <Image
-              src={portraitImage}
-              alt=""
-              sizes="(min-width: 1024px) 32rem, 20rem"
-              className="aspect-square rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
-            />
+    <div className="rounded-4xl bg-custom-white py-20">
+      <Container>
+        <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12 bg-transparent">
+          <div className="lg:pl-20">
+            <div className="max-w-xs px-2.5 lg:max-w-none">
+              <Image
+                src={portraitImage}
+                alt=""
+                sizes="(min-width: 1024px) 32rem, 20rem"
+                className="aspect-square rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+              />
+            </div>
+          </div>
+          <div className="lg:order-first lg:row-span-2">
+            <h1 className="text-4xl font-bold tracking-tight text-custom-red sm:text-5xl dark:text-zinc-100">
+              SOBRE A THAI
+            </h1>
+            <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+              <p>
+                A Thai Suítes está localizada na Vila de Itamambuca, em Ubatuba – litoral norte de São Paulo, a 700m de umas das mais belas praias do Brasil.
+              </p>
+              <p>
+                Este refúgio de paz e tranquilidade é rodeado de natureza e oferece um escape da rotina e do stress do dia-a-dia. É o lugar perfeito para quem gosta de se hospedar com estilo, conforto e paz!
+              </p>
+              <p>
+                São 8 suítes com TV, Wi-fi, Ar condicionado e Frigobar (4 standards, 2 masters e 2 premiums com cozinha privativa).
+              </p>
+            </div>
           </div>
         </div>
-        <div className="lg:order-first lg:row-span-2">
-          <h1 className="text-4xl font-bold tracking-tight text-custom-red sm:text-5xl dark:text-zinc-100">
-            SOBRE A THAI
-          </h1>
-          <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
-            <p>
-              A Thai Suítes está localizada na Vila de Itamambuca, em Ubatuba – litoral norte de São Paulo, a 700m de umas das mais belas praias do Brasil.
-            </p>
-            <p>
-              Este refúgio de paz e tranquilidade é rodeado de natureza e oferece um escape da rotina e do stress do dia-a-dia. É o lugar perfeito para quem gosta de se hospedar com estilo, conforto e paz!
-            </p>
-            <p>
-              São 8 suítes com TV, Wi-fi, Ar condicionado e Frigobar (4 standards, 2 masters e 2 premiums com cozinha privativa).
-            </p>
-          </div>
-        </div>
-      </div>
-    </Container>
+      </Container>
+    </div>
   )
 }
