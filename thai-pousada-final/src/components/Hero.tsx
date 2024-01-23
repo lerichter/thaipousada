e5c8d8eda3f0881501018dba5589a99c10757cd4
lerichter <lerichter.c@gmail.com>
2024-01-22 21:43:14 -0300

    carousel

--- thai-pousada-final/src/components/Hero.tsx
+++ thai-pousada-final/src/components/Hero.tsx
@@ -1,38 +1,110 @@
-import Image from 'next/image'
-// Importação de outros componentes, se necessário
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 
-import hero from '@/images/photos/itamambuca4.jpg' //2 ou 3 .. escolher
-import logoThai from '@/images/photos/logo-thai.png'
+// Supondo que logoThai também esteja na pasta public
+import logoThai from '@/images/logos/Logotipo_Thai-03.png';
+import image1 from '@/images/photos/itamambuca1.jpg';
+import image2 from '@/images/photos/itamambuca2.jpg';
+import image3 from '@/images/photos/itamambuca3.jpg';
+import image4 from '@/images/photos/itamambuca4.jpg';
+
+const images = [
+  {
+    id: 1,
+    image: image1,
+    alt: 'Imagem 1'
+  },
+  {
+    id: 2,
+    image: image2,
+    alt: 'Imagem 2'
+  },
+  {
+    id: 3,
+    image: image3,
+    alt: 'Imagem 3'
+  },
+  {
+    id: 4,
+    image: image4,
+    alt: 'Imagem 4'
+  },
+];
 
 export function Hero() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const goToSlide = (slideIndex: React.SetStateAction<number>) => {
+    setCurrentSlide(slideIndex);
+  };
+
+  const goToNextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % images.length);
+  };
+
+  const goToPrevSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide - 1 + images.length) % images.length);
+  };
+
+  useEffect(() => {
+    // Define um intervalo para mudar para o próximo slide
+    const slideInterval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % images.length);
+    }, 3000); // Muda a imagem a cada 5 segundos
+
+    // Limpa o intervalo quando o componente é desmontado
+    return () => clearInterval(slideInterval);
+  }, []);
+
   return (
-    <div className="w-screen h-screen text-white">
-      <div className="relative h-screen w-full">
-        <Image src={hero} alt="Background Image" className="absolute inset-0 w-full h-full object-cover filter" />
-        <div className="absolute inset-0 flex flex-col items-center justify-center">
-          {/* Logo Thai Image */}
-          <Image src={logoThai} alt="Thai Pousada Logo" className="w-auto h-32" />
-          <p className="mt-6 text-lg text-center text-white bold">O lugar perfeito para quem gosta de se hospedar com estilo, conforto e tranquilidade.</p>
-        </div>
+    <div id="default-carousel" className="relative w-full" data-carousel="slide">
+      <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`duration-700 ease-in-out ${index === currentSlide ? 'block' : 'hidden'}`}
+            data-carousel-item
+          >
+            <Image
+              src={image.image}
+              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              alt={image.alt}
+            />
+          </div>
+        ))}
       </div>
-      {/* <Photos /> */}
+      <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+        {images.map((_, index) => (
+          <button
+            key={index}
+            type="button"
+            className={`w-3 h-3 rounded-full bg-custom-red ${index === currentSlide ? 'opacity-100' : 'opacity-50'}`}
+            aria-current={index === currentSlide ? "true" : "false"}
+            aria-label={`Slide ${index + 1}`}
+            onClick={() => goToSlide(index)}
+          ></button>
+        ))}
+      </div>
+      <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onClick={goToPrevSlide} data-carousel-prev>
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+            <svg className="w-4 h-4 text-custom-red rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+            </svg>
+            <span className="sr-only">Previous</span>
+        </span>
+      </button>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-40 px-4 max-w-xs md:max-w-sm lg:max-w-md">
+        <Image src={logoThai} alt="Logo" layout="responsive" width={300} height={150} />
+        {/* <p className="mt-6 text-lg text-center text-white bold">O lugar perfeito para quem gosta de se hospedar com estilo, conforto e tranquilidade.</p> */}
+      </div>
+      <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onClick={goToNextSlide} data-carousel-next>
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+          <svg className="w-4 h-4 text-custom-red rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+          </svg>
+          <span className="sr-only">Next</span>
+        </span>
+      </button>
     </div>
   );
 }
-
-//  export function Hero() {
-//   return (
-//     <><Container className="mt-9">
-//       <div className="max-w-4xl">
-//         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-//           Thai Pousada - Itamambuca
-//         </h1>
-//         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-//           Inspirada na arquitetura do Sudeste da Ásia, a Thai Suítes possui oito suítes decoradas com muito charme e exclusividade.
-//           <br></br> É o lugar perfeito para quem gosta se hospedar com estilo, conforto e tranquilidade.
-
-//         </p>
-//       </div>
-//     </Container><Photos /></>
-//   );
-//  }
--- thai-pousada-final/src/components/Suites.tsx
+++ thai-pousada-final/src/components/Suites.tsx
@@ -13,25 +13,25 @@ const suitesData = [
     id: 1,
     title: 'BALI - PREMIUM',
     description: 'A suíte premium Bali está localizada no térreo, com uma varanda aconchegante de uso privativo com vista para o jardim. Na varanda possui rede para descanso, um sofá e uma mesa para refeições e afins. No interior da suíte possui uma cama de solteiro, uma cama de casal com uma auxiliar de solteiro embaixo, smart TV, ar-condicionado e frigobar. A suíte também conta com uma cozinha equipada com utensílios, fogão e micro-ondas.',
-    icon: suite1,
+    image: suite1,
   },
   {
     id: 2,
     title: 'JBAY - PREMIUM',
     description: 'A suíte premium Jeffreys Bay está localizada no piso superior, com uma varanda de uso privativo com vista para o jardim. No interior da suíte possui uma cama de solteiro e uma cama de casal com uma auxiliar de solteiro embaixo. Além de Smart TV, ar-condicionado e frigobar, a suíte também possui em seu interior uma cozinha equipada com utensílios, fogão, micro-ondas e liquidificador.',
-    icon: suite1,
+    image: suite1,
   },
   {
     id: 3,
     title: 'ILHA BELA - MASTER',
     description: 'A suíte master Ilha Bela está localizada no piso superior e possui varanda com vista para o jardim. No interior da suíte possuem duas camas de solteiro e uma cama de casal com uma auxiliar de solteiro embaixo. A suíte é equipada com frigobar, smart TV e ar-condicionado.',
-    icon: suite1,
+    image: suite1,
   },
   {
     id: 4,
     title: 'ITAMAMBUCA - MASTER',
     description: 'A suíte master Itamambuca está localizada no térreo, com uma varanda aconchegante de uso privativo com vista para o jardim. Na varanda contém rede para descanso, um sofá e uma mesa para refeições e afins. No interior da suíte possuem duas camas de solteiro e uma cama de casal com uma auxiliar de solteiro em baixo. A suíte é equipada com frigobar, smart TV e ar-condicionado.',
-    icon: suite1,
+    image: suite1,
   },
 ];
 
@@ -47,7 +47,7 @@ export function Suites() {
             <div key={suite.id} className="flex flex-col justify-between h-full bg-custom-red p-4 rounded-lg shadow-md">
               <div>
                 <div className="w-32 h-32 bg-white p-1 overflow-hidden">
-                <Image className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center" alt="hero" src={suite1} layout='responsive'/>
+                  <Image className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center" alt="hero" src={suite1} layout='responsive'/>
                 </div>
                 <h1 className="mt-4 text-lg text-black font-semibold">{suite.title}</h1>
                 <p className="text-white mt-2 text-xs">{suite.description}</p>
