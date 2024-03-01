cfeaa63d4afba2af5170f610dd936f78ee270670
lerichter <lerichter.c@gmail.com>
2024-03-01 16:40:32 -0300

    sistema de reservas

--- thai-pousada-final/src/components/Widget.tsx
+++ thai-pousada-final/src/components/Widget.tsx
@@ -1,9 +1,7 @@
-/* eslint-disable @next/next/no-sync-scripts */
-
 import { Container } from '../components/Container2'
+import Script from 'next/script'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-// Importar o estilo CSS para personalizar o widget
 import '@/styles/customWidgetStyles.css' 
 
 export function Widget() {
@@ -12,7 +10,6 @@ export function Widget() {
       <div className="fixed bottom-0 left-0 w-full z-40 bg-blue rounded-4xl">
         <Container>
           <div className="flex flex-wrap justify-between items-center text-white py-4">
-            {/* O script do widget permanece o mesmo */}
             <script src="https://hotels.cloudbeds.com/widget/load/bPfQcZ/float?newWindow=1"></script>
           </div>
         </Container>
